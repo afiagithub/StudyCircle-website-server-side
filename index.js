4c98704354d9204fe63e6d89e46aa4eca2fa74fa
Afia Rahman <rahmanmou26@gmail.com>
2024-05-10 16:56:41 +0600

    created api for fetching single assignment data by id

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ async function run() {
         const result = await cursor.toArray()
         res.send(result);
     })
+
+    app.get("/all-assignment/:id", async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await assignmentCollection.findOne(query);
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

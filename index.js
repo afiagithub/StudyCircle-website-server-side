a5685c95d68bec486706b38f614a94d68fa6c8a5
Afia Rahman <rahmanmou26@gmail.com>
2024-05-10 16:13:00 +0600

    added fake json data to database and fetched data to server

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const assignmentCollection = client.db("studyDB").collection("assignments")
+
+    app.get("/all-assignment", async(req, res) => {
+        const cursor = assignmentCollection.find()
+        const result = await cursor.toArray()
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

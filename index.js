c5a0ac51d1a37ba800abd92c725aad6e5cb0059a
Afia Rahman <rahmanmou26@gmail.com>
2024-05-11 19:16:52 +0600

    created api to update pending assignment

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors({
-    origin: ["http://localhost:5173", "https://study-circle-auth.web.app", "https://study-circle-auth.firebaseapp.com"],
-    credentials: true
-  }))
+  origin: ["http://localhost:5173", "https://study-circle-auth.web.app", "https://study-circle-auth.firebaseapp.com"],
+  credentials: true
+}))
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctn12zm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -31,77 +31,91 @@ async function run() {
     const assignmentCollection = client.db("studyDB").collection("assignments");
     const submitCollection = client.db("studyDB").collection("submissions");
 
-    app.get("/all-assignment", async(req, res) => {
-        const cursor = assignmentCollection.find()
-        const result = await cursor.toArray()
-        res.send(result);
+    app.get("/all-assignment", async (req, res) => {
+      const cursor = assignmentCollection.find()
+      const result = await cursor.toArray()
+      res.send(result);
     })
 
-    app.get("/all-assignment/:id", async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await assignmentCollection.findOne(query);
-        res.send(result)
+    app.get("/all-assignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await assignmentCollection.findOne(query);
+      res.send(result)
     })
 
-    app.post("/all-assignment", async(req, res) => {
-        const assignmentData = req.body;
-        const result = await assignmentCollection.insertOne(assignmentData)
-        res.send(result)
+    app.post("/all-assignment", async (req, res) => {
+      const assignmentData = req.body;
+      const result = await assignmentCollection.insertOne(assignmentData)
+      res.send(result)
     })
 
-    app.put("/all-assignment/:id", async(req, res) => {
-        const id = req.params.id;
-        const filter = { _id: new ObjectId(id) };
-        const options = { upsert: true };
-        const updatedAssignment = req.body;
+    app.put("/all-assignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedAssignment = req.body;
 
-        const updatedData = {
-            $set: {
-                ...updatedAssignment
-            }
+      const updatedData = {
+        $set: {
+          ...updatedAssignment
         }
-        const result = await assignmentCollection.updateOne(filter, updatedData, options)
-        res.send(result)
+      }
+      const result = await assignmentCollection.updateOne(filter, updatedData, options)
+      res.send(result)
     })
 
-    app.delete("/all-assignment/:id", async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await assignmentCollection.deleteOne(query);
-        res.send(result)
+    app.delete("/all-assignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await assignmentCollection.deleteOne(query);
+      res.send(result)
     })
 
-    app.get("/posted/:email", async(req, res) => {
-        const email = req.params.email;
-        const query = {'a_creator.email': email}
-        const result = await assignmentCollection.find(query).toArray()
-        res.send(result)
+    app.get("/posted/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { 'a_creator.email': email }
+      const result = await assignmentCollection.find(query).toArray()
+      res.send(result)
     })
 
-    app.post("/submission", async(req, res) => {
+    app.post("/submission", async (req, res) => {
       const submitData = req.body;
       const result = await submitCollection.insertOne(submitData)
       res.send(result)
     })
 
-    app.get("/submission/:id", async(req, res) => {
+    app.get("/submission/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await submitCollection.findOne(query)
       res.send(result)
     })
 
-    app.get("/attempted/:email", async(req, res) => {
+    app.put("/submission/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedSubmission = req.body;
+      const updatedData = {
+        $set: {
+          ...updatedSubmission
+        }
+      }
+      const result = await submitCollection.updateOne(filter, updatedData, options)
+      res.send(result)
+    })
+
+    app.get("/attempted/:email", async (req, res) => {
       const email = req.params.email;
-      const query = {'submitter.email': email}
+      const query = { 'submitter.email': email }
       const result = await submitCollection.find(query).toArray()
       res.send(result)
     })
 
-    app.get("/pending/:email", async(req, res) => {
+    app.get("/pending/:email", async (req, res) => {
       const email = req.params.email;
-      const query = {creator_email : email}
+      const query = { creator_email: email }
       const result = await submitCollection.find(query).toArray()
       res.send(result)
     })
